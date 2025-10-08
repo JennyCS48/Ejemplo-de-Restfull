e95cc63f27a7f6aca2500449f1f918ac7ffd7aca
Jennifer CS <csjenny20@gmail.com>
2025-10-08 10:14:55 -0500

    carpeta productos y sus subcarpetas creadas / ProductoController.java creada

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/web/rest/ProductoController.java
@@ -0,0 +1,11 @@
+package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// se obtiene todo a partir de productos
+@RequestMapping("/productos")
+public class ProductoController {
+
+}
