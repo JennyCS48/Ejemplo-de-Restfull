24ecaf86ab439ed0cd39f2a49a8c278828f6305b
Jennifer CS <csjenny20@gmail.com>
2025-10-20 12:29:02 -0500

    pequeña modificacion en ProductoRepository

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/domain/repository/ProductoRepository.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/domain/repository/ProductoRepository.java
@@ -3,6 +3,6 @@ package es.etg.daw.dawes.java.rest.restfull.productos.domain.repository;
 import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.Producto;
 import java.util.Optional;
 
-public interface ProductoRepository extends CRUDRepository {
+public interface ProductoRepository extends CRUDRepository<Producto, Integer> {
     public Optional<Producto> getByName(String name);
 }
