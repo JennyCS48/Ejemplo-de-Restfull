89da868ce6e5ab8706f7b8475aab7c9914ef2026
Jennifer CS <csjenny20@gmail.com>
2025-10-21 13:44:11 -0500

    FindProducto y Updateproducto actualizads con las excepciones

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/usecase/FindProductoUseCase.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/usecase/FindProductoUseCase.java
@@ -1,9 +1,8 @@
 package es.etg.daw.dawes.java.rest.restfull.productos.application.usecase;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.error.ProductoNotFoundException;
 import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.Producto;
 import es.etg.daw.dawes.java.rest.restfull.productos.domain.repository.ProductoRepository;
 import lombok.AllArgsConstructor;
@@ -12,15 +11,17 @@ import lombok.RequiredArgsConstructor;
 @AllArgsConstructor
 public class FindProductoUseCase {
 
-private ProductoRepository productoRepository;
+    private ProductoRepository productoRepository;
 
-
- public List<Producto> findAll(){
-        //TODO Habría que implementarlo con un acceso real a los datos. 
+    public List<Producto> findAll() {
+        // TODO Habría que implementarlo con un acceso real a los datos.
         // lo vamos a simular
-        List<Producto> lista = new ArrayList<>();
-       
-        return lista = productoRepository.getAll();
+        List<Producto> productos = productoRepository.getAll();
+
+        if (productos.isEmpty())
+            throw new ProductoNotFoundException();
+
+        return productos;
 
     }
 }
--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/usecase/UpdateProductoUseCase.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/usecase/UpdateProductoUseCase.java
@@ -3,6 +3,7 @@ package es.etg.daw.dawes.java.rest.restfull.productos.application.usecase;
 import java.time.LocalDateTime;
 
 import es.etg.daw.dawes.java.rest.restfull.productos.application.command.EditProductoCommand;
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.error.ProductoNotFoundException;
 import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.Producto;
 import es.etg.daw.dawes.java.rest.restfull.productos.domain.repository.ProductoRepository;
 import lombok.AllArgsConstructor;
@@ -12,20 +13,18 @@ public class UpdateProductoUseCase {
 
     private ProductoRepository productoRepository;
 
+    public Producto update(EditProductoCommand command) {
 
-    public Producto update(EditProductoCommand command){
+        // TODO habria que buscar primero el producto
+        // TODO y luego mapeariamos los nuevos valores
 
-        //TODO habria que buscar primero el producto
-        //TODO y luego mapeariamos los nuevos valores
+        return productoRepository.getById(command.id())
+                .map(p -> { // Actualizamos los atributos del objeto
+                    p.setNombre(command.nombre());
+                    p.setPrecio(command.precio());
+                    return productoRepository.save(p);
+                })
+                .orElseThrow(() -> new ProductoNotFoundException(command.id()));
 
-
-        Producto producto = Producto.builder()
-                                    .id(command.id())
-									.nombre(command.nombre())
-									.precio(command.precio())
-									.createdAt(LocalDateTime.now()).build();
-
-        productoRepository.save(producto);
-        return producto;
-}
+    }
 }
