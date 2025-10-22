85b4afb855bd7a1ea3bca08dc96e5c21f6e5855a
Jennifer CS <csjenny20@gmail.com>
2025-10-22 10:23:47 -0500

    metodo que captura errores y devuelve un mapa con el campo que o cumple (controlador)

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/web/rest/ProductoController.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/web/rest/ProductoController.java
@@ -1,6 +1,7 @@
 package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.web.rest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.etg.daw.dawes.java.rest.restfull.productos.application.command.CreateProductoCommand;
@@ -16,54 +17,74 @@ import es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.web.dto.Pro
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @RestController
-@RequestMapping("/productos") //La url será /productos
+@RequestMapping("/productos") // La url será /productos
 @RequiredArgsConstructor
 public class ProductoController {
-private final CreateProductoService createProductoService;
-// Nuevo atributo
-private final FindProductoService findProductoService;
-private final DeleteProductoService deleteProductoService;
-private final UpdateProductoService updateProductoService;
+    private final CreateProductoService createProductoService;
+    // Nuevo atributo
+    private final FindProductoService findProductoService;
+    private final DeleteProductoService deleteProductoService;
+    private final UpdateProductoService updateProductoService;
 
-	@PostMapping //Método Post
-	public ResponseEntity<ProductoResponse> createProducto(@Valid @RequestBody ProductoRequest productoRequest) {
-		CreateProductoCommand comando = ProductoMapper.toCommand(productoRequest); 
-		Producto producto = createProductoService.createProducto(comando);
-		return ResponseEntity.status(HttpStatus.CREATED).body(ProductoMapper.toResponse(producto)); //Respuestagit@github.com:julparper/dawes-springboot-restful.git
-	}
-	@GetMapping
-    public List<ProductoResponse> allProductos(){
+    @PostMapping // Método Post
+    public ResponseEntity<ProductoResponse> createProducto(@Valid @RequestBody ProductoRequest productoRequest) {
+        CreateProductoCommand comando = ProductoMapper.toCommand(productoRequest);
+        Producto producto = createProductoService.createProducto(comando);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ProductoMapper.toResponse(producto)); // Respuestagit@github.com:julparper/dawes-springboot-restful.git
+    }
+
+    @GetMapping
+    public List<ProductoResponse> allProductos() {
 
         return findProductoService.findAll()
-                .stream() //Convierte la lista en un flujo
-                .map(ProductoMapper::toResponse) //Mapeamos/Convertimos cada elemento del flujo (Producto) en un objeto de Respuesta (ProductoResponse)
-                .toList(); //Lo devuelve como una lista.
+                .stream() // Convierte la lista en un flujo
+                .map(ProductoMapper::toResponse) // Mapeamos/Convertimos cada elemento del flujo (Producto) en un objeto
+                                                 // de Respuesta (ProductoResponse)
+                .toList(); // Lo devuelve como una lista.
 
     }
 
-	@DeleteMapping("/{id}")
-    public ResponseEntity<?>  deleteProducto(@PathVariable int id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProducto(@PathVariable int id) {
         deleteProductoService.delete(id);
-        return ResponseEntity.noContent().build(); //Devpñvemos una respuesta vacía.
+        return ResponseEntity.noContent().build(); // Devpñvemos una respuesta vacía.
     }
 
-     @PutMapping("/{id}")
-    public ProductoResponse editProducto(@PathVariable int id, @RequestBody ProductoRequest productoRequest){
+    @PutMapping("/{id}")
+    public ProductoResponse editProducto(@PathVariable int id, @RequestBody ProductoRequest productoRequest) {
         EditProductoCommand comando = ProductoMapper.toCommand(id, productoRequest);
         Producto producto = updateProductoService.update(comando);
-        return  ProductoMapper.toResponse(producto); //Respuesta
+        return ProductoMapper.toResponse(producto); // Respuesta
+    }
+
+    // Método que captura los errores y devuelve un mapa con el campo que no cumple
+    // la validación y un mensaje de error.
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
