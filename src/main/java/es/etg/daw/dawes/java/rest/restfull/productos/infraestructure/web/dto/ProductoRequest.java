ae8f8a38a3e0b9ed4fa6125659567651509d6a8f
Jennifer CS <csjenny20@gmail.com>
2025-10-22 10:03:30 -0500

    clase productorequest con validaciones

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/web/dto/ProductoRequest.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/web/dto/ProductoRequest.java
@@ -1,5 +1,17 @@
 package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.web.dto;
 
-public record ProductoRequest(String nombre, double precio) {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
+public record ProductoRequest(
+                  // El nombre no puede estar en blanco
+                @NotBlank 
+                    String nombre, 
+                  // El precio tiene que estar entre 0 y 100
+                @Min(value = 0) 
+                @Max(value = 100)
+                    double precio) {
+
+    
 }
