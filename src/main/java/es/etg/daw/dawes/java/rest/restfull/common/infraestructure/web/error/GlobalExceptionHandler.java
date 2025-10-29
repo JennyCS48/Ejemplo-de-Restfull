878f5aca3947462beae129b4256a3c4373c3d1e7
Jennifer CS <csjenny20@gmail.com>
2025-10-29 10:59:31 -0500

    clase GlobalExceptionHandler creada vacia

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/common/infraestructure/web/error/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package es.etg.daw.dawes.java.rest.restfull.common.infraestructure.web.error;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+//Indicamos que es una gestora de errores
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+}
