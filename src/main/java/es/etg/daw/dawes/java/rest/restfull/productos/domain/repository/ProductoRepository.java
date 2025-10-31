813be9c58279898ac86b5cda47ac62de4809cc82
Jennifer CS <csjenny20@gmail.com>
2025-10-31 10:29:57 -0500

    CRUDRepository movido a Common/domain/repository y productorepository y ccategoriarepository creadas en productos/domain/repository

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/domain/repository/ProductoRepository.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/domain/repository/ProductoRepository.java
@@ -1,8 +1,12 @@
 package es.etg.daw.dawes.java.rest.restfull.productos.domain.repository;
 
+import es.etg.daw.dawes.java.rest.restfull.common.domain.repository.CRUDRepository;
 import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.Producto;
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.ProductoId;
+
 import java.util.Optional;
 
-public interface ProductoRepository extends CRUDRepository<Producto, Integer> {
+public interface ProductoRepository extends CRUDRepository<Producto, ProductoId> {
+    
     public Optional<Producto> getByName(String name);
 }
