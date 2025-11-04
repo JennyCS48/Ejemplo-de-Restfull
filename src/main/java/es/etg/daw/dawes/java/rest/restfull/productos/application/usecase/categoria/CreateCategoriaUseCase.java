51cee85a893454a3992d236bada88603fc1ff0b4
Jennifer CS <csjenny20@gmail.com>
2025-11-04 13:24:00 -0600

    usecases de categoria hechos

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/usecase/categoria/CreateCategoriaUseCase.java
@@ -0,0 +1,23 @@
+package es.etg.daw.dawes.java.rest.restfull.productos.application.usecase.categoria;
+
+import java.time.LocalDateTime;
+import es.etg.daw.dawes.java.rest.restfull.productos.application.command.categoria.CreateCategoriaCommand;
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.Categoria;
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.repository.CategoriaRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CreateCategoriaUseCase {
+    private CategoriaRepository categoriaRepository;
+
+	public Categoria create(CreateCategoriaCommand comando) {
+
+		Categoria categoria = Categoria.builder()
+				.nombre(comando.nombre())
+				.createdAt(LocalDateTime.now()).build();
+
+		categoriaRepository.save(categoria); // base de datos.
+		return categoria;
+
+	}
+}
---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/usecase/categoria/EditCategoriaUseCase.java
@@ -0,0 +1,23 @@
+package es.etg.daw.dawes.java.rest.restfull.productos.application.usecase.categoria;
+
+import es.etg.daw.dawes.java.rest.restfull.productos.application.command.categoria.EditCategoriaCommand;
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.error.ProductoNotFoundException;
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.Categoria;
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.repository.CategoriaRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class EditCategoriaUseCase {
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria update(EditCategoriaCommand command) {
+
+        return categoriaRepository.getById(command.id())
+                .map(p -> { // Actualizamos los atributos del objeto
+                    p.setNombre(command.nombre());
+                    return categoriaRepository.save(p);
+                })
+                .orElseThrow(() -> new ProductoNotFoundException(command.id().getValue()));
+
+    }
+}
