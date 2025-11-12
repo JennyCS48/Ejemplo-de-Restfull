2e538f721b01c2e2f8c2b6a36585dc21eda3ba4c
Jennifer CS <csjenny20@gmail.com>
2025-11-12 09:57:07 -0600

    CategoriaRepositoryMockImplTest.java hecha

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/db/repository/mock/categoria/CategoriaFactory.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/db/repository/mock/categoria/CategoriaFactory.java
@@ -17,4 +17,8 @@ public class CategoriaFactory {
         
         return datos;
     }
+
+    public static Categoria create() {
+        return new Categoria(new CategoriaId(1), "Nombre categoria nueva", LocalDateTime.now());
+    }
 }
---
+++ src/test/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/db/repository/mock/categoria/CategoriaRepositoryMockImplTest.java
@@ -0,0 +1,46 @@
+package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.db.repository.mock.categoria;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.Categoria;
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.repository.CategoriaRepository;
+
+public class CategoriaRepositoryMockImplTest {
+    CategoriaRepository repository = new CategoriaRepositoryMockImpl();
+
+    @BeforeEach
+    void setUp() {
+        // Inicializamos las el repositorio
+        repository = new CategoriaRepositoryMockImpl();
+
+    }
+
+    @Test
+    void save(){
+        var categoria = CategoriaFactory.create();
+      
+        Categoria c = repository.save(categoria);
+
+        assertAll(
+                () -> assertNotNull(c), // el producto no es nulo
+                () -> assertNotNull(c.getId()), // el producto creado tiene id
+                () -> assertNotNull(repository.getById(c.getId())) // si lo busco lo debo recuperar *opcional
+        );
+        
+    }
+
+    @Test
+    void getAll() {
+        var categorias = repository.getAll();
+
+        assertAll(
+                () -> assertNotNull(categorias),
+                () -> assertEquals(CategoriaFactory.getDemoData().size(), categorias.size())
+        );
+    }
+}
