2f0ff839dfdd5b5078c9eee4003dc8b95fa7dd3f
Jennifer CS <csjenny20@gmail.com>
2025-11-20 10:36:22 -0600

    productoentity  y categoriaentity imports actualizados

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/db/jpa/entity/CategoriaEntity.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/db/jpa/entity/CategoriaEntity.java
@@ -3,10 +3,17 @@ package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.db.jpa.ent
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 
 @Entity
 @Data
--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/db/jpa/entity/ProductoEntity.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/db/jpa/entity/ProductoEntity.java
@@ -1,12 +1,20 @@
 package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.db.jpa.entity;
 
-import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Entity
 @Data
