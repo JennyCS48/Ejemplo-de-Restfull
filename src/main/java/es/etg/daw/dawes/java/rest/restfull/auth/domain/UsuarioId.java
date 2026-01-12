58a74e5fa8288d6d6ee4339745fc5254ed6d7c6a
Jennifer CS <csjenny20@gmail.com>
2026-01-12 12:37:06 -0600

    nuevo vs auth, domain - UsuarioId creada

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/auth/domain/UsuarioId.java
@@ -0,0 +1,9 @@
+package es.etg.daw.dawes.java.rest.restfull.auth.domain;
+
+import es.etg.daw.dawes.java.rest.restfull.common.domain.model.Identificador;
+
+public class UsuarioId extends Identificador{
+    protected UsuarioId(Integer value) {
+        super(value);
+    }
+}
